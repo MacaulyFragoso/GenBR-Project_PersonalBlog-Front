8aef08c1489cc1580d3943b3b1fa4446d7a83251
MacaulyFragoso <macaulyaparecido@gmail.com>
2021-11-05 14:30:59 -0300

    [Feature Macauly - Add Model | 05.11.21]

---
+++ src/app/model/PostModel.ts
@@ -0,0 +1,11 @@
+import { ThemeModel } from "./ThemeModel";
+import { UserModel } from "./UserModel";
+
+export class PostModel {
+    public id: number;
+    public titulo: string;
+    public texto: string;
+    public date: Date;
+    public usuario: UserModel;
+    public tema: ThemeModel[];
+}
