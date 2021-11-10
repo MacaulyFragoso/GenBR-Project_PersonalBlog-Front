cdb08724416c5465bddde3109766a2318b55c1a4
MacaulyFragoso <macaulyaparecido@gmail.com>
2021-11-10 12:48:00 -0300

    [Feature Macauly - Task: Update Cadastrar & Entrar | 10.11.21]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../model/UserModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  userModel: UserModel = new UserModel();
+  confirmaSenha: string;
+  tipoUser: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+
+    window.scrollTo(0,0);
+
+  }
+
+  confirmarSenha(event: any) {
+    this.confirmaSenha = event.target.value;
+
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUser = event.target.value;
+  }
+
+  cadastrar(){
+    this.userModel.tipo = this.tipoUser;
+
+    if(this.userModel.senha != this.confirmaSenha){
+      alert("Senhas não conferem");
+
+    }else{
+      this.authService.cadastrar(this.userModel).subscribe((resp: UserModel) => {
+        this.userModel = resp;
+        this.router.navigate(['/entrar']);
+          alert("Cadastro realizado com sucesso!");
+    })
+
+  }
+
   }
 
 }
